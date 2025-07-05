c8cd49942fc2b98a16559c49c2e6bf89d014472c
MathieuC <oomtm453@gmail.com>
2025-07-05 18:00:34 -0400

    Changed version to 0.12.0.

--- Ruleset/Ruleset.cs
+++ Ruleset/Ruleset.cs
@@ -23,7 +23,7 @@ namespace oomtm450PuckMod_Ruleset {
         /// <summary>
         /// Const string, version of the mod.
         /// </summary>
-        private const string MOD_VERSION = "0.12.0DEV8";
+        private const string MOD_VERSION = "0.12.0";
 
         /// <summary>
         /// Const float, radius of the puck.
