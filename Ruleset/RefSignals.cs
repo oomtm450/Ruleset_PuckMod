85a932d63ea8dd4a33d857b410c578c8ead80335
MathieuC <oomtm453@gmail.com>
2025-07-05 14:17:39 -0400

    Update client-sided ref signals.

--- Ruleset/RefSignals.cs
+++ Ruleset/RefSignals.cs
@@ -89,6 +89,7 @@ namespace oomtm450PuckMod_Ruleset {
                         rectTransform.sizeDelta = new Vector2(300, 300);
                         rectTransform.pivot = new Vector2(0.5f, 0.5f);
                         rectTransform.anchorMin = new Vector2(0.925f, 0.42f);
+                        rectTransform.anchorMax = new Vector2(0.925f, 0.42f);
 
                         _images.Add(fileName, image);
                     }
--- Ruleset/Ruleset.cs
+++ Ruleset/Ruleset.cs
@@ -23,7 +23,7 @@ namespace oomtm450PuckMod_Ruleset {
         /// <summary>
         /// Const string, version of the mod.
         /// </summary>
-        private const string MOD_VERSION = "0.12.0DEV3";
+        private const string MOD_VERSION = "0.12.0DEV4";
 
         /// <summary>
         /// Const float, radius of the puck.
