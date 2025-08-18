15919b24949a650b9bbdc807fda7b0499e73d0ee
MathieuC <oomtm453@gmail.com>
2025-08-18 18:14:46 -0400

    Fix to codebase.

--- Codebase/Properties/AssemblyInfo.cs
+++ Codebase/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("Codebase")]
+[assembly: AssemblyTitle("oomtm450PuckMod_Codebase")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Codebase")]
+[assembly: AssemblyProduct("oomtm450PuckMod_Codebase")]
 [assembly: AssemblyCopyright("Copyright © Mathieu Cody 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
