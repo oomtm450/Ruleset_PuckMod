5058cac49a47aa08cdcaf604ad960c0ecb6d4951
MathieuC <oomtm453@gmail.com>
2025-08-25 14:34:24 -0400

    Fixed offside logic.

--- Ruleset/Ruleset.cs
+++ Ruleset/Ruleset.cs
@@ -1206,7 +1206,7 @@ namespace oomtm450PuckMod_Ruleset {
                     if (oldZone == lastPlayerOnPuckTeamZones[2] && _puckZone == lastPlayerOnPuckTeamZones[0]) {
                         PlayerTeam lastPlayerOnPuckOtherTeam = TeamFunc.GetOtherTeam(_lastPlayerOnPuckTeam);
                         foreach (string key in new List<string>(_isOffside.Keys)) {
-                            if (_isOffside[key].Team == lastPlayerOnPuckOtherTeam)
+                            if (_isOffside[key].Team == lastPlayerOnPuckOtherTeam && _lastPlayerOnPuckTeamTipIncluded == lastPlayerOnPuckOtherTeam)
                                 _isOffside[key] = (lastPlayerOnPuckOtherTeam, false);
                         }
                     }
