d34fabf34c6a26bcc680e690c44df37a9d433e57
MathieuC <oomtm453@gmail.com>
2025-09-08 18:16:14 -0400

    Adjusted icing values.

--- Ruleset/Ruleset.cs
+++ Ruleset/Ruleset.cs
@@ -1717,7 +1717,7 @@ namespace oomtm450PuckMod_Ruleset {
                                 if (!player)
                                     continue;
 
-                                float maxPossibleTimeLimit = ((float)((GetDistance(puck.Rigidbody.transform.position.x, puck.Rigidbody.transform.position.z, player.PlayerBody.transform.position.x, player.PlayerBody.transform.position.z) * 265d) + 9000d)) - (Math.Abs(player.PlayerBody.transform.position.z) * 340f);
+                                float maxPossibleTimeLimit = ((float)((GetDistance(puck.Rigidbody.transform.position.x, puck.Rigidbody.transform.position.z, player.PlayerBody.transform.position.x, player.PlayerBody.transform.position.z) * 270d) + 9000d)) - (Math.Abs(player.PlayerBody.transform.position.z) * 340f);
                                 Logging.Log($"Possible time is : {maxPossibleTime}. Limit is : {maxPossibleTimeLimit}. Puck Y is : {puck.Rigidbody.transform.position.y}.", _serverConfig, true); // TODO TEST REMOVE
 
                                 if (maxPossibleTime >= maxPossibleTimeLimit) {
