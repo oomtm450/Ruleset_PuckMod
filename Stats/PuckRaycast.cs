3d52547e79c50c1c4caa65a58f24afad090ad2ee
MathieuC <oomtm453@gmail.com>
2025-09-18 18:52:42 -0400

    Optimized and adjusted puck raycasts.

--- Stats/PuckRaycast.cs
+++ Stats/PuckRaycast.cs
@@ -9,9 +9,10 @@ namespace oomtm450PuckMod_Stats {
     /// Class containing code for the puck raycasts with the goal trigger to know if the puck is going towards the net or not.
     /// </summary>
     internal class PuckRaycast : MonoBehaviour {
-        private const int CHECK_EVERY_X_FRAMES = 8;
+        private const int CHECK_EVERY_X_FRAMES = 6;
         //private readonly Vector3 TOP_VECTOR = new Vector3(0, 0.175f, 0);
-        private readonly float RIGHT_COORDINATE = Codebase.Constants.PUCK_RADIUS + 0.018f;
+        private readonly Vector3 ABOVE_GROUND_VECTOR = new Vector3(0, 0.001f, 0);
+        private readonly float RIGHT_COORDINATE = Codebase.Constants.PUCK_RADIUS + 0.019f;
         private readonly Vector3 BOTTOM_VECTOR = new Vector3(0, -0.6f, 0);
         private readonly float MAX_DISTANCE = 26f;
         private readonly LayerMask _goalTriggerlayerMask = GetLayerMask("Goal Trigger"); // 15
@@ -59,17 +60,6 @@ namespace oomtm450PuckMod_Stats {
                 foreach (PlayerTeam key in new List<PlayerTeam>(PuckIsGoingToNet.Keys))
                     PuckIsGoingToNet[key] = false;
 
-                _startingPosition.y = transform.position.y; // Adjust Y of starting position so that the rays are all parallel to the ice.
-
-                Vector3 normalizedDirection = (transform.position - _startingPosition).normalized;
-
-                Vector3 leftVector = Vector3.Cross(Vector3.down, normalizedDirection).normalized;
-                Vector3 rightVector = Vector3.Cross(Vector3.up, normalizedDirection).normalized;
-
-                _rayBottomLeft = new Ray((transform.position + leftVector * RIGHT_COORDINATE) + new Vector3(0, 0.001f, 0), transform.position - _startingPosition);
-                _rayBottomRight = new Ray((transform.position + rightVector * RIGHT_COORDINATE) + new Vector3(0, 0.001f, 0), transform.position - _startingPosition);
-                _rayFarBottomLeft = new Ray((transform.position + leftVector * RIGHT_COORDINATE) + BOTTOM_VECTOR, transform.position - _startingPosition);
-                _rayFarBottomRight = new Ray((transform.position + rightVector * RIGHT_COORDINATE) + BOTTOM_VECTOR, transform.position - _startingPosition);
                 CheckForColliders();
 
                 ResetStartingPosition();
@@ -85,6 +75,13 @@ namespace oomtm450PuckMod_Stats {
         }
 
         private void CheckForColliders() {
+            _startingPosition.y = transform.position.y; // Adjust Y of starting position so that the rays are all parallel to the ice.
+
+            Vector3 direction = transform.position - _startingPosition;
+
+            Vector3 leftVector = transform.position + Vector3.Cross(Vector3.down, direction.normalized).normalized * RIGHT_COORDINATE;
+            Vector3 rightVector = transform.position + Vector3.Cross(Vector3.up, direction.normalized).normalized * RIGHT_COORDINATE;
+
             /*_lineRendererBottomLeft.SetPosition(0, _rayBottomLeft.origin);
             _lineRendererBottomLeft.SetPosition(1, _rayBottomLeft.origin + (_rayBottomLeft.direction * MAX_DISTANCE));
             _lineRendererBottomLeft.material = _noHitMaterial;
@@ -101,12 +98,16 @@ namespace oomtm450PuckMod_Stats {
             _lineRendererFarBottomRight.SetPosition(1, _rayFarBottomRight.origin + (_rayFarBottomRight.direction * MAX_DISTANCE));
             _lineRendererFarBottomRight.material = _noHitMaterial;*/
 
+            _rayBottomLeft = new Ray(leftVector + ABOVE_GROUND_VECTOR, direction);
             bool hasHit = Physics.Raycast(_rayBottomLeft, out RaycastHit hit, MAX_DISTANCE, _goalTriggerlayerMask, QueryTriggerInteraction.Collide);
             if (!hasHit) {
+                _rayBottomRight = new Ray(rightVector + ABOVE_GROUND_VECTOR, direction);
                 hasHit = Physics.Raycast(_rayBottomRight, out hit, MAX_DISTANCE, _goalTriggerlayerMask, QueryTriggerInteraction.Collide);
                 if (!hasHit) {
+                    _rayFarBottomLeft = new Ray(leftVector + BOTTOM_VECTOR, direction);
                     hasHit = Physics.Raycast(_rayFarBottomLeft, out hit, MAX_DISTANCE, _goalTriggerlayerMask, QueryTriggerInteraction.Collide);
                     if (!hasHit) {
+                        _rayFarBottomRight = new Ray(rightVector + BOTTOM_VECTOR, direction);
                         hasHit = Physics.Raycast(_rayFarBottomRight, out hit, MAX_DISTANCE, _goalTriggerlayerMask, QueryTriggerInteraction.Collide);
                         if (!hasHit)
                             return;
