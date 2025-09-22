365f586b6760a5b70e5a3f29698d9039ce7a7a58
MathieuC <oomtm453@gmail.com>
2025-09-21 20:42:10 -0400

    Adjusted icing values.

--- Ruleset/Configs/ServerConfig.cs
+++ Ruleset/Configs/ServerConfig.cs
@@ -269,12 +269,12 @@ namespace oomtm450PuckMod_Ruleset.Configs {
         /// Dictionary of Zone and float, number of milliseconds after puck exiting the stick before arriving behind the goal line to not be considered for icing for each zone.
         /// </summary>
         public Dictionary<Zone, float> MaxPossibleTime { get; set; } = new Dictionary<Zone, float> {
-            { Zone.BlueTeam_BehindGoalLine, 8750f },
-            { Zone.RedTeam_BehindGoalLine, 8750f },
-            { Zone.BlueTeam_Zone, 7000f },
-            { Zone.RedTeam_Zone, 7000f },
-            { Zone.BlueTeam_Center, 5250f },
-            { Zone.RedTeam_Center, 5250f },
+            { Zone.BlueTeam_BehindGoalLine, 9000f },
+            { Zone.RedTeam_BehindGoalLine, 9000f },
+            { Zone.BlueTeam_Zone, 7250f },
+            { Zone.RedTeam_Zone, 7250f },
+            { Zone.BlueTeam_Center, 5500f },
+            { Zone.RedTeam_Center, 5500f },
         };
 
         /// <summary>
