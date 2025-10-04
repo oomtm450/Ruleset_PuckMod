63ff40837cc53a8cc4b8ff06664c6bbfeb1c8b38
MathieuC <oomtm453@gmail.com>
2025-10-04 01:54:04 -0400

    Minor improvement to code.

--- Sounds/SoundsSystem.cs
+++ Sounds/SoundsSystem.cs
@@ -81,7 +81,7 @@ namespace oomtm450PuckMod_Sounds {
                     return true;
                 }
 
-                Logging.Log($"LoadSounds launching GetAudioClips. ({fullPath})", Sounds.ClientConfig);
+                Logging.Log($"{nameof(LoadSounds)} launching {nameof(GetAudioClips)}. ({fullPath})", Sounds.ClientConfig);
                 StartCoroutine(GetAudioClips(fullPath, setCustomGoalHorns));
             }
             catch (Exception ex) {
