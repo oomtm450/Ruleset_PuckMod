b96491edf15a3353c1f5650bfc09b9e5f47a5f17
MathieuC <oomtm453@gmail.com>
2025-10-16 15:27:04 -0400

    Adjusted high stick values.

--- Ruleset/Ruleset.cs
+++ Ruleset/Ruleset.cs
@@ -595,7 +595,7 @@ namespace oomtm450PuckMod_Ruleset {
                             _noHighStickFrames.Add(currentPlayerSteamId, noHighStickFrames);
                         }
 
-                        if (noHighStickFrames >= ServerManager.Instance.ServerConfigurationManager.ServerConfiguration.serverTickRate / 15) {
+                        if (noHighStickFrames >= ServerManager.Instance.ServerConfigurationManager.ServerConfiguration.serverTickRate / 18) {
                             _isHighStickActiveTimers.TryGetValue(stick.Player.Team.Value, out Timer highStickTimer);
 
                             highStickTimer.Change(_serverConfig.HighStick.MaxMilliseconds, Timeout.Infinite);
