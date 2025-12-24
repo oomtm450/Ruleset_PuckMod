9270b4e9ef2ac6c6a87b962bfba4bcbda8fa567d
MathieuC <oomtm453@gmail.com>
2025-12-23 21:48:06 -0500

    Changed Stats version to 0.7.0 and Ruleset to 0.27.0.

--- Stats/Stats.cs
+++ Stats/Stats.cs
@@ -20,7 +20,7 @@ namespace oomtm450PuckMod_Stats {
         /// <summary>
         /// Const string, version of the mod.
         /// </summary>
-        private static readonly string MOD_VERSION = "0.7.0DEV7";
+        private static readonly string MOD_VERSION = "0.7.0";
 
         /// <summary>
         /// List of string, last released versions of the mod.
