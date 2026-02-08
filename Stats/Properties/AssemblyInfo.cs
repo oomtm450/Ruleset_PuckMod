49b70be093c1e21788d733d806ece4315cc70760
MathieuC <oomtm453@gmail.com>
2026-02-07 20:48:14 -0500

    Changed Stats version to 0.7.2.

--- Stats/Properties/AssemblyInfo.cs
+++ Stats/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Numéro de build
 //      Révision
 //
-[assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyVersion("0.7.2.0")]
+[assembly: AssemblyFileVersion("0.7.2.0")]
