3cc6cc44fc31d813d95744bd495229aebfcb5d0a
MathieuC <oomtm453@gmail.com>
2026-02-16 18:01:08 -0500

    Changed Ruleset version to 0.28.0.

--- Ruleset/Ruleset.cs
+++ Ruleset/Ruleset.cs
@@ -27,7 +27,7 @@ namespace oomtm450PuckMod_Ruleset {
         /// <summary>
         /// Const string, version of the mod.
         /// </summary>
-        private static readonly string MOD_VERSION = "0.28.0DEV";
+        private static readonly string MOD_VERSION = "0.28.0";
 
         /// <summary>
         /// ReadOnlyCollection of string, last released versions of the mod.
