675757d01941651a0f0d4cd7cdeaec08698e8fa5
MathieuC <oomtm453@gmail.com>
2026-02-21 22:28:02 -0500

    Fixes to penalties.

--- Ruleset/Penalty.cs
+++ Ruleset/Penalty.cs
@@ -10,7 +10,7 @@ namespace oomtm450PuckMod_Ruleset {
         private const int MAX_SAME_PLAYER_PENALTY_COUNT = 2;
         private const int MAX_PENALIZED_PLAYERS = 2;
 
-        private static readonly Vector3 BLUE_PENALTY_BOX_POSITION = new Vector3(26f, 1.2f, 3f);
+        private static readonly Vector3 BLUE_PENALTY_BOX_POSITION = new Vector3(26f, 1f, 3f);
 
         private static readonly Vector3 RED_PENALTY_BOX_POSITION = new Vector3(BLUE_PENALTY_BOX_POSITION.x, BLUE_PENALTY_BOX_POSITION.y, BLUE_PENALTY_BOX_POSITION.z * -1);
 
