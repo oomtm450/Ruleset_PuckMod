de795c9f3aebf806d719edc6d0dbba711a679462
MathieuC <oomtm453@gmail.com>
2026-03-09 17:37:06 -0400

    Fixed penalty timer position when spectating.

--- Ruleset/Ruleset.cs
+++ Ruleset/Ruleset.cs
@@ -3298,7 +3298,7 @@ namespace oomtm450PuckMod_Ruleset {
             penaltiesLabel.style.unityTextOutlineWidth = referenceLabel.resolvedStyle.unityTextOutlineWidth;
             penaltiesLabel.style.textShadow = new StyleTextShadow(StyleKeyword.Auto);
 
-            penaltiesLabel.style.bottom = new Length(96.5f, LengthUnit.Percent);
+            penaltiesLabel.style.bottom = new Length(397f, LengthUnit.Percent);
             if (!blue)
                 penaltiesLabel.style.marginLeft = new Length(100f - ClientConfig.RedTeamPenaltyTimerXOffset, LengthUnit.Percent);
         }
