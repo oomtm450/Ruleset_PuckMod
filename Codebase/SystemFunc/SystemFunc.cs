18f2abdc3f55b9262c9d9da2fdae1039334efd10
MathieuC <oomtm453@gmail.com>
2026-03-09 22:26:43 -0400

    Refactoring.

--- Codebase/SystemFunc/SystemFunc.cs
+++ Codebase/SystemFunc/SystemFunc.cs
@@ -35,53 +35,5 @@ namespace Codebase {
                 .Where(c => !Char.IsWhiteSpace(c))
                 .ToArray());
         }
-
-        public class StreamString {
-            private readonly Stream _ioStream;
-            private readonly UnicodeEncoding _streamEncoding;
-
-            public bool IsConnected {
-                get {
-                    if (_ioStream is NamedPipeClientStream clientIOStream)
-                        return clientIOStream.IsConnected;
-                    if (_ioStream is NamedPipeServerStream serverIOStream)
-                        return serverIOStream.IsConnected;
-
-                    return false;
-                }
-            }
-
-            public StreamString(Stream ioStream) {
-                _ioStream = ioStream;
-                _streamEncoding = new UnicodeEncoding();
-            }
-
-            public string ReadString() {
-                int len = _ioStream.ReadByte() * 256;
-                len += _ioStream.ReadByte();
-                byte[] inBuffer = new byte[len];
-                _ioStream.Read(inBuffer, 0, len);
-
-                return _streamEncoding.GetString(inBuffer);
-            }
-
-            public int WriteString(string outString) {
-                byte[] outBuffer = _streamEncoding.GetBytes(outString);
-                int len = outBuffer.Length;
-                if (len > UInt16.MaxValue)
-                    len = (int)UInt16.MaxValue;
-
-                _ioStream.WriteByte((byte)(len / 256));
-                _ioStream.WriteByte((byte)(len & 255));
-                _ioStream.Write(outBuffer, 0, len);
-                _ioStream.Flush();
-
-                return outBuffer.Length + 2;
-            }
-
-            public void Close() {
-                _ioStream.Close();
-            }
-        }
     }
 }
