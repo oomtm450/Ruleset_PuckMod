64dc5fe3f05f892f1887d7fbef101d4f2c9d3ac7
MathieuC <oomtm453@gmail.com>
2026-05-31 20:50:52 -0400

    Adjusted penalty timers.

--- Ruleset/Ruleset.cs
+++ Ruleset/Ruleset.cs
@@ -3457,7 +3457,7 @@ namespace oomtm450PuckMod_Ruleset {
             penaltiesLabel.style.unityTextOutlineWidth = referenceLabel.resolvedStyle.unityTextOutlineWidth;
             penaltiesLabel.style.textShadow = new StyleTextShadow(StyleKeyword.Auto);
 
-            penaltiesLabel.style.top = new Length(96.8f, LengthUnit.Percent);
+            penaltiesLabel.style.top = new Length(97f, LengthUnit.Percent);
             if (!blue)
                 penaltiesLabel.style.marginLeft = new Length(100f - ClientConfig.RedTeamPenaltyTimerXOffset, LengthUnit.Percent);
             else
