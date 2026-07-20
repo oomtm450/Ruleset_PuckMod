bff68eaa7679e6995fed61733019630fe739b861
MathieuC <oomtm453@gmail.com>
2026-07-20 17:31:44 -0400

    Adjusted goalie interference values.

--- Ruleset/Configs/OldServerConfig.cs
+++ Ruleset/Configs/OldServerConfig.cs
@@ -408,12 +408,12 @@ namespace oomtm450PuckMod_Ruleset.Configs {
         /// <summary>
         /// Float, force threshold for a push on the goalie to be considered for goalie interference.
         /// </summary>
-        public float CollisionForceThreshold { get; } = 0.9695f;
+        public float CollisionForceThreshold { get; } = 0.9695f;  // TODO : Change after release.
 
         /// <summary>
         /// Float, radius of a goalie. Make higher to augment the crease size for goalie interference calls.
         /// </summary>
-        public float GoalieRadius { get; } = 0.81f;
+        public float GoalieRadius { get; } = 0.81f;  // TODO : Change after release.
 
         /// <summary>
         /// Method that updates this config with the new default values, if the old default values were used.
--- Ruleset/Configs/ServerConfig.cs
+++ Ruleset/Configs/ServerConfig.cs
@@ -906,12 +906,12 @@ namespace oomtm450PuckMod_Ruleset.Configs {
         /// <summary>
         /// Float, force threshold for a push on the goalie to be considered for goalie interference.
         /// </summary>
-        public float CollisionForceThreshold { get; set; } = 0.9695f;
+        public float CollisionForceThreshold { get; set; } = 0.97f;
 
         /// <summary>
         /// Float, radius of a goalie. Make higher to augment the crease size for goalie interference calls.
         /// </summary>
-        public float GoalieRadius { get; set; } = 0.81f;
+        public float GoalieRadius { get; set; } = 0.82f;
 
         #region Constructors
         /// <summary>
