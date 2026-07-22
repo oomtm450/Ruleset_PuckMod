41c054512d56e7ffe62085cdd73bcb3eae72d49d
MathieuC <oomtm453@gmail.com>
2026-07-22 16:05:39 -0400

    Adjusted charging values.

--- Ruleset/Configs/ServerConfig.cs
+++ Ruleset/Configs/ServerConfig.cs
@@ -439,7 +439,7 @@ namespace oomtm450PuckMod_Ruleset.Configs {
         /// <summary>
         /// Float, skater's speed threshold to call a charging penalty.
         /// </summary>
-        public float ChargingSpeedThreshold { get; set; } = 20f;
+        public float ChargingSpeedThreshold { get; set; } = 8.52f;
 
         /// <summary>
         /// Int, skater's last sprint timespan threshold to call a charging penalty in milliseconds.
