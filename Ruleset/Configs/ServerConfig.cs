2696a6edc53414c8d19c64548117e00d7aee6d09
MathieuC <oomtm453@gmail.com>
2026-07-26 11:52:03 -0400

    Adjusted gint values.

--- Ruleset/Configs/ServerConfig.cs
+++ Ruleset/Configs/ServerConfig.cs
@@ -950,12 +950,12 @@ namespace oomtm450PuckMod_Ruleset.Configs {
         /// <summary>
         /// Float, force threshold for a push on the goalie to be considered for goalie interference.
         /// </summary>
-        public float CollisionForceThreshold { get; set; } = 0.97f;
+        public float CollisionForceThreshold { get; set; } = 0.969f;
 
         /// <summary>
         /// Float, radius of a goalie. Make higher to augment the crease size for goalie interference calls.
         /// </summary>
-        public float GoalieRadius { get; set; } = 0.82f;
+        public float GoalieRadius { get; set; } = 0.821f;
 
         #region Constructors
         /// <summary>
