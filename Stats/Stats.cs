47154eea38ecd4b0ed8b7020f931bc3928aa260f
MathieuC <oomtm453@gmail.com>
2026-08-03 17:25:55 -0400

    Fixed stars on OT goal.

--- Stats/Stats.cs
+++ Stats/Stats.cs
@@ -1878,7 +1878,7 @@ namespace oomtm450PuckMod_Stats {
 
             Dictionary<string, double> starPoints = new Dictionary<string, double>();
             foreach (Player player in PlayerManager.Instance.GetPlayers()) {
-                if (!PlayerFunc.IsPlayerPlaying(player) || (player.Team != PlayerTeam.Blue && player.Team != PlayerTeam.Red) || player.PlayerPosition == null || !player.PlayerPosition ||
+                if (!player || player.Role == PlayerRole.None || (player.Team != PlayerTeam.Blue && player.Team != PlayerTeam.Red) || player.PlayerPosition == null || !player.PlayerPosition ||
                     string.IsNullOrEmpty(player.PlayerPosition.Name))
                     continue;
 
