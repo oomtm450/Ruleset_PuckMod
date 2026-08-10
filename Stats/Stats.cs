d5c5184e46a7bd41450db36cea25d08375b16d5f
MathieuC <oomtm453@gmail.com>
2026-08-10 14:23:45 -0400

    Adjusted goalie stars logic.

--- Stats/Stats.cs
+++ Stats/Stats.cs
@@ -1893,7 +1893,7 @@ namespace oomtm450PuckMod_Stats {
 
                 if (PlayerFunc.IsGoalie(player) || player.PlayerPosition.Name == PlayerFunc.GOALIE_POSITION) {
                     if (_savePerc.TryGetValue(steamId, out var saveValues))
-                        starPoints[steamId] += (((double)saveValues.Saves) / ((double)saveValues.Shots)) - 0.250d * ((double)saveValues.Saves) * 45d;
+                        starPoints[steamId] += (((double)saveValues.Saves) / ((double)saveValues.Shots)) - 0.250d * ((double)saveValues.Saves) * 50d;
 
                     if (_sog.TryGetValue(steamId, out int shots))
                         starPoints[steamId] += ((double)shots) * 1d;
