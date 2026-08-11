752dae623f4c91b8e1ed7915f0efdd700383019f
MathieuC <oomtm453@gmail.com>
2026-08-11 19:14:49 -0400

    Added commands in help.

--- Ruleset/Ruleset.cs
+++ Ruleset/Ruleset.cs
@@ -1640,7 +1640,7 @@ namespace oomtm450PuckMod_Ruleset {
                         content = content.ToLowerInvariant();
 
                         if (content.StartsWith(@"/help"))
-                            SystemFunc.AddClientChatMessage("Ruleset commands:\n* <b>/refscale</b> - Change the scale of the 2D refs images (0.0-2.0)\n* <b>/voteref</b> - Vote to become ref\n* <b>/voteremoveref</b> - Vote to remove a ref (number, name or SteamId)\n");
+                            SystemFunc.AddClientChatMessage("Ruleset commands:\n* <b>/refscale</b> - Change the scale of the 2D refs images (0.0-2.0)\n* <b>/voteref</b> - Vote to become ref\n* <b>/voteremoveref</b> - Vote to remove a ref (number, name or SteamId)\n* <b>REF UI</b> - F8 to open\n");
                     }
                 }
                 catch (Exception ex) {
