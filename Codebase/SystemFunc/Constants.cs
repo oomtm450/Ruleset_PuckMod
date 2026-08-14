f106f59d8659b0c09f23cc6297ca96b3464ee154
MathieuC <oomtm453@gmail.com>
2026-08-14 13:18:12 -0400

    Adjusted for new Puck version 1235.

--- Codebase/SystemFunc/Constants.cs
+++ Codebase/SystemFunc/Constants.cs
@@ -3,7 +3,7 @@
         /// <summary>
         /// Const string, added current Puck Application.version to check for mod compatibility.
         /// </summary>
-        internal const string CURRENT_APPLICATION_VERSION = "1153";
+        internal const string CURRENT_APPLICATION_VERSION = "1235";
 
         /// <summary>
         /// Const string, prefix of all the mod's names.
