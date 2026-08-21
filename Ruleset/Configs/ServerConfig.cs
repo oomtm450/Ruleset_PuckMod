24f454d37f055dcb814ad107a8b92cc1e3011299
MathieuC <oomtm453@gmail.com>
2026-08-20 22:25:02 -0400

    Adjusted tipping values.

--- Ruleset/Configs/ServerConfig.cs
+++ Ruleset/Configs/ServerConfig.cs
@@ -83,7 +83,7 @@ namespace oomtm450PuckMod_Ruleset.Configs {
         /// <summary>
         /// Int, number of milliseconds for a puck to not be considered tipped by a player's stick.
         /// </summary>
-        public int MaxTippedMilliseconds { get; set; } = 32;
+        public int MaxTippedMilliseconds { get; set; } = 31;
 
         /// <summary>
         /// Float, puck speed tipping ratio.
