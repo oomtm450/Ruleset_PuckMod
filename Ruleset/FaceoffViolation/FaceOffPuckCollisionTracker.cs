710a02b0f2f1902d3284644f4c4e3a4b3fbead75
MathieuC <oomtm453@gmail.com>
2026-08-23 14:02:38 -0400

    Fixed penalty given on faceoff violation as human ref.

--- Ruleset/FaceoffViolation/FaceOffPuckCollisionTracker.cs
+++ Ruleset/FaceoffViolation/FaceOffPuckCollisionTracker.cs
@@ -150,7 +150,7 @@ namespace oomtm450PuckMod_Ruleset.FaceoffViolation {
 
             // Check if player has hit the penalty threshold.
             if (violation.ViolationCount >= Ruleset.ServerConfig.Faceoff.MaxViolationsBeforePenalty) {
-                if (!Ruleset.ServerConfig.Penalty.FaceoffViolation || !PenaltyModule.GivePenalty(PenaltyType.FaceoffViolation, violatingPlayer, "", referee)) {
+                if ((!Ruleset.ServerConfig.Penalty.FaceoffViolation && Ruleset.CurrentRefMode == RefMode.AI) || !PenaltyModule.GivePenalty(PenaltyType.FaceoffViolation, violatingPlayer, "", referee)) {
                     // Send penalty chat message.
                     Ruleset.SystemChatMessages.Add(
                         $"PENALTY: #{violatingPlayer.Number.Value} {violatingPlayer.Username.Value} has {Ruleset.ServerConfig.Faceoff.MaxViolationsBeforePenalty} faceoff violations! Will be frozen after spawn."
